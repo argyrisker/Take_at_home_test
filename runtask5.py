66d9c19a891b4984ef32c68c1ada7bc787382782
Argyrios <70695741+argyrisker@users.noreply.github.com>
2026-06-05 18:22:11 +0200

    Update data and weights folder paths

--- runtask5.py
+++ runtask5.py
@@ -2,7 +2,7 @@ from Task5 import evaluate
 
 
 if __name__ == "__main__":
-    data_folder = "/projects/mai/se_mai/users/kjzj855_argyrios/project/TakeHomeTest/data"
-    weights_folder = "/projects/mai/se_mai/users/kjzj855_argyrios/project/TakeHomeTest"
+    data_folder = "/project/TakeHomeTest/data"
+    weights_folder = "/project/TakeHomeTest"
 
     evaluate(data_folder, weights_folder, threshold=0.5)
